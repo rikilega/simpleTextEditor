1a6c3ef00f712e73cc18770c378b5753f33a8a72
rikilega <rosedoland33@yahoo.com>
2023-08-17 05:00:22 -0700

    modified service worker and package.json for fron and backend, as well aas server.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -60,4 +60,3 @@ registerRoute(
   })
 );
 
-registerRoute();
--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path'); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 require('./routes/htmlRoutes')(app);
 app.get('*', (req, res) => {
